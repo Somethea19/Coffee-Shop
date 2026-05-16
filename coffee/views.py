1f017b356175bfd329ab151031fa230174a31339
Somethea19 <sometheaoeung45@gmail.com>
2026-05-16 21:43:36 +0700

    first commit

---
+++ coffee/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Coffee
+
+# Create your views here.
+def home(request):
+    coffee = Coffee.objects.all()
+    return render(request,'home.html',{'coffee': coffee})
+def about(request):
+    return render(request,'about.html')
+def category(request):
+    return render(request,'category.html')
+def contact(request):
+    return render(request,'contact.html')
+def popular(request):
+    return render(request,'popular.html')
