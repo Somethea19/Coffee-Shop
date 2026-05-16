d6c5507e6f87ff29a41116ae4204a6619dea5323
Somethea19 <sometheaoeung45@gmail.com>
2026-05-17 06:12:00 +0700

    Prepare for deployment

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8-$x%^n-k3bxqvq22ou8co&6c0j+pv2f6^k6n5g%+$*b3ql4wc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
